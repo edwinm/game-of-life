e63111705c35d4033872244354f557b63d62a9c5
Edwin Martin <edwin@bitstorm.org>
2020-06-11 17:01:29 +0200

    Clean up game of life algorithm

--- src/components/gameoflife.ts
+++ src/components/gameoflife.ts
@@ -1,76 +1,79 @@
 interface Neighbours {
-  n: number;
+  neighbours: number;
   cell: Cell;
   populated: boolean;
 }
 
 export function gofNext(shape: Cell[]) {
-  let neighbours = <Neighbours>{};
+  let neighbourCells = <Neighbours[]>[];
   let newShape = <Cell[]>[];
+
   shape.forEach((cell) => {
     let index: string;
 
     index = `${cell.x - 1},${cell.y - 1}`;
-    if (neighbours[index]) {
-      neighbours[index].n++;
+    if (neighbourCells[index]) {
+      neighbourCells[index].neighbours++;
     } else {
-      neighbours[index] = {n: 1, cell: {x: cell.x - 1, y: cell.y - 1}};
+      neighbourCells[index] = {neighbours: 1, cell: {x: cell.x - 1, y: cell.y - 1}};
     }
     index = `${cell.x},${cell.y - 1}`;
-    if (neighbours[index]) {
-      neighbours[index].n++;
+    if (neighbourCells[index]) {
+      neighbourCells[index].neighbours++;
     } else {
-      neighbours[index] = {n: 1, cell: {x: cell.x, y: cell.y - 1}};
+      neighbourCells[index] = {neighbours: 1, cell: {x: cell.x, y: cell.y - 1}};
     }
     index = `${cell.x + 1},${cell.y - 1}`;
-    if (neighbours[index]) {
-      neighbours[index].n++;
+    if (neighbourCells[index]) {
+      neighbourCells[index].neighbours++;
     } else {
-      neighbours[index] = {n: 1, cell: {x: cell.x + 1, y: cell.y - 1}};
+      neighbourCells[index] = {neighbours: 1, cell: {x: cell.x + 1, y: cell.y - 1}};
     }
     index = `${cell.x - 1},${cell.y}`;
-    if (neighbours[index]) {
-      neighbours[index].n++;
+    if (neighbourCells[index]) {
+      neighbourCells[index].neighbours++;
     } else {
-      neighbours[index] = {n: 1, cell: {x: cell.x - 1, y: cell.y}};
+      neighbourCells[index] = {neighbours: 1, cell: {x: cell.x - 1, y: cell.y}};
     }
     index = `${cell.x + 1},${cell.y}`;
-    if (neighbours[index]) {
-      neighbours[index].n++;
+    if (neighbourCells[index]) {
+      neighbourCells[index].neighbours++;
     } else {
-      neighbours[index] = {n: 1, cell: {x: cell.x + 1, y: cell.y}};
+      neighbourCells[index] = {neighbours: 1, cell: {x: cell.x + 1, y: cell.y}};
     }
     index = `${cell.x - 1},${cell.y + 1}`;
-    if (neighbours[index]) {
-      neighbours[index].n++;
+    if (neighbourCells[index]) {
+      neighbourCells[index].neighbours++;
     } else {
-      neighbours[index] = {n: 1, cell: {x: cell.x - 1, y: cell.y + 1}};
+      neighbourCells[index] = {neighbours: 1, cell: {x: cell.x - 1, y: cell.y + 1}};
     }
     index = `${cell.x},${cell.y + 1}`;
-    if (neighbours[index]) {
-      neighbours[index].n++;
+    if (neighbourCells[index]) {
+      neighbourCells[index].neighbours++;
     } else {
-      neighbours[index] = {n: 1, cell: {x: cell.x, y: cell.y + 1}};
+      neighbourCells[index] = {neighbours: 1, cell: {x: cell.x, y: cell.y + 1}};
     }
     index = `${cell.x + 1},${cell.y + 1}`;
-    if (neighbours[index]) {
-      neighbours[index].n++;
+    if (neighbourCells[index]) {
+      neighbourCells[index].neighbours++;
     } else {
-      neighbours[index] = {n: 1, cell: {x: cell.x + 1, y: cell.y + 1}};
-    }
-  });
-  shape.forEach(function (cell, i) {
-    const index = `${cell.x},${cell.y}`;
-    if (neighbours[index]) {
-      neighbours[index].populated = true;
+      neighbourCells[index] = {neighbours: 1, cell: {x: cell.x + 1, y: cell.y + 1}};
     }
   });
 
-  for (const index in neighbours) {
-    if ((neighbours[index].n == 2 && neighbours[index].populated) || neighbours[index].n == 3) {
-      newShape.push(neighbours[index].cell);
+  shape.forEach(function (cell) {
+    const neighbour = neighbourCells[`${cell.x},${cell.y}`];
+    if (neighbour && neighbour.neighbours == 2) {
+      neighbour.neighbours = 3;
+    }
+  });
+
+  for (const index in neighbourCells) {
+    if (neighbourCells[index].neighbours == 3) {
+      newShape.push(neighbourCells[index].cell);
     }
   }
+
   return newShape;
 }
 
