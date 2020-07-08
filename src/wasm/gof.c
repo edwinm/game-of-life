fcb047b6daa8c473a477f8c8416ca061575969e2
Edwin Martin <edwin@bitstorm.org>
2020-07-08 15:08:30 +0200

    Registering neighbours

--- src/wasm/gof.c
+++ src/wasm/gof.c
@@ -1,13 +1,40 @@
+/*
+	Game of Life C implementation
+	Copyright 2020 Edwin Martin
+	Published under MIT license
+*/
+
 #include <stdio.h>
 #include <emscripten/emscripten.h>
 
-int gof(int* input_ptr, int* output_ptr, int len){
-	int i;
-	for(i = 0; i < len; i++) {
-    output_ptr[i] = input_ptr[i] + 1;
-	}
-	output_ptr[i++] = 99;
-	output_ptr[i++] = 99;
-	output_ptr[i++] = 99;
-  return i;
+struct neighbours_struct {
+	int x;
+	int y;
+	int count;
+};
+
+int* input;
+struct neighbours_struct* neighbours;
+int neighbours_count;
+
+void push(int x, int y) {
+	struct neighbours_struct* current = &neighbours[neighbours_count];
+	current->x = x;
+	current->y = y;
+	current->count = 1;
+	neighbours_count++;
 }
+
+int gof(int* input_ptr, struct neighbours_struct* output_ptr, int len) {
+	neighbours_count = 0;
+	neighbours = output_ptr;
+	int i;
+	for(i = 0; i < len; i+=2) {
+		push(input_ptr[i], input_ptr[i+1]);
+	}
+
+	return len * 1.5;
+}
+
+
+
