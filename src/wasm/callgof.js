d944180b6a9c5b31ea3301047d108259c3cf376f
Edwin Martin <edwin@bitstorm.org>
2020-07-09 15:31:18 +0200

    Merge and compress with closure compiler

--- src/wasm/callgof.js
+++ src/wasm/callgof.js
@@ -1,37 +1,37 @@
-const gof = Module.cwrap("gof", "number", ["number", "number", "number"]);
+var gof = Module["cwrap"]("gof", "number", ["number", "number", "number"]);
 
 function callGof(cells) {
-  const inputArray = new Int32Array(cells);
-  const len = inputArray.length;
-  const bytesPerElement = inputArray.BYTES_PER_ELEMENT;
+  var inputArray = new Int32Array(cells);
+  var len = inputArray.length;
+  var bytesPerElement = inputArray.BYTES_PER_ELEMENT;
 
-  const inputPtr = Module._malloc(len * bytesPerElement * 8); // 8 neighbours
-  const outputPtr = Module._malloc(len * bytesPerElement * 12); // 8 neighbours / 2 * 3
+  var inputPtr = Module["_malloc"](len * bytesPerElement * 8); // 8 neighbours
+  var outputPtr = Module["_malloc"](len * bytesPerElement * 12); // 8 neighbours / 2 * 3
 
-  Module.HEAP32.set(inputArray, inputPtr / bytesPerElement);
+  Module["HEAP32"]["set"](inputArray, inputPtr / bytesPerElement);
 
-  const newSize = gof(inputPtr, outputPtr, len / 2) * 2;
-  const inputArray2 = [
-    ...new Int32Array(Module.HEAP32.buffer, inputPtr, newSize),
-  ];
+  var newSize = gof(inputPtr, outputPtr, len / 2) * 2;
+  // var inputArray2 = [
+  //   ...new Int32Array(Module.HEAP32.buffer, inputPtr, newSize),
+  // ];
 
-  Module._free(inputPtr);
-  Module._free(outputPtr);
+  // Module._free(inputPtr);
+  Module["_free"](outputPtr);
 
-  return inputArray2;
+  return new Int32Array(Module["HEAP32"].buffer, inputPtr, newSize);
 }
 
-export function doGof(shape) {
-  const ret = [];
-  const arr = callGof(shape.flat());
-  for (let i = 0; i < arr.length; i += 2) {
+function doGof(shape) {
+  var ret = [];
+  var arr = callGof(shape.flat());
+  for (var i = 0; i < arr.length; i += 2) {
     ret.push([arr[i], arr[i + 1]]);
   }
   return ret;
 }
 
-setTimeout(() => {
-  const glider = [
+setTimeout(function () {
+  var glider = [
     [1, 0],
     [2, 1],
     [2, 2],
@@ -39,6 +39,6 @@ setTimeout(() => {
     [0, 2],
   ];
 
-  const nextGen = doGof(glider);
+  var nextGen = doGof(glider);
   console.log("nextGen", nextGen);
 }, 1000);
--- src/wasm/hello.c
+++
@@ -1,3 +0,0 @@
-#include <emscripten/emscripten.h>
-
-EMSCRIPTEN_KEEPALIVE int add(x, y) { return x + y; }
