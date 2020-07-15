c0aa5d559f25f961ac169aa9365b8ec2a5333014
Edwin Martin <edwin@bitstorm.org>
2020-07-15 22:42:18 +0200

    Working reverse

--- src/test/plain.c
+++ src/test/plain.c
@@ -1,7 +1,21 @@
-#include <emscripten/emscripten.h>
+#include <stdlib.h>
+#include <emscripten.h>
+
+//EMSCRIPTEN_KEEPALIVE
+//void *malloc(unsigned long);
 
 EMSCRIPTEN_KEEPALIVE
-int fib() {
-  return 42;
+void *mem(int size) {
+	return malloc(size);
+}
+
+EMSCRIPTEN_KEEPALIVE
+int fib(int a) {
+//  int *ptr = 16384L;
+//  ptr[0] = 99;
+
+//	emscripten_run_script("callJs");
+
+  return 45 + a;
 }
 
