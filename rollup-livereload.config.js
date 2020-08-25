29e64bb8063224df5ae91f8a937a2a79cdb0bf34
Edwin Martin <edwin@bitstorm.org>
2020-08-25 20:16:45 +0200

    Config continue watch after typescript error

--- rollup-livereload.config.js
+++ rollup-livereload.config.js
@@ -1,13 +1,13 @@
-import typescript from '@rollup/plugin-typescript';
-import livereload from 'rollup-plugin-livereload';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import typescript from "@rollup/plugin-typescript";
+import livereload from "rollup-plugin-livereload";
+import nodeResolve from "rollup-plugin-node-resolve";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
-    file: 'dist/bundle.min.js',
-    format: 'iife',
-    name: 'bundle',
+    file: "dist/bundle.min.js",
+    format: "iife",
+    name: "bundle",
   },
-  plugins: [nodeResolve(), typescript(), livereload()],
+  plugins: [nodeResolve(), typescript({ noEmitOnError: false }), livereload()],
 };
