30eeca151d2a5b5bd15eba85d8848a98f5885936
Edwin Martin <edwin@bitstorm.org>
2026-01-04 15:25:39 +0100

    Count translations using Sentry

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,33 @@ import { GolControls } from "./web-components/controls";
 import { Shape } from "./components/shape";
 import { routeListener } from "./components/routelistener";
 import { GolButton } from "./web-components/button";
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://ba196a3eefba43bb9747fdf793d32776@o224348.ingest.us.sentry.io/5373220",
+});
+
+setTimeout(() => {
+  Sentry.metrics.count("lang", 1, {
+    attributes: {
+      version: "1.0.0",
+      navigatorLanguage: navigator.language,
+      topLanguage: navigator.languages?.[0],
+      start: (
+        document
+          .querySelector("gol-controls")
+          ?.shadowRoot.getElementById("start")
+          ?.shadowRoot.querySelector("slot[class=visible]") as HTMLSlotElement
+      )?.assignedElements()?.[0].textContent,
+      clear: (
+        document
+          .querySelector("gol-controls")
+          ?.shadowRoot.getElementById("clear")
+          ?.shadowRoot.querySelector("slot[class=visible]") as HTMLSlotElement
+      )?.assignedElements()?.[0].textContent,
+    },
+  });
+}, 500);
 
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = <GolCanvas>$("gol-canvas");
